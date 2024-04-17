f380ebcb3ddaed71cf652f4dbe5be0a141d29af4
Gonzalo-MR8 <gonzamarreg@gmail.com>
2024-04-17 12:36:42 +0200

    Initial Commit

---
+++ RyMZara/RyMZaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  RyMZaraApp.swift
+//  RyMZara
+//
+//  Created by GonzaloMR on 17/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RyMZaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
